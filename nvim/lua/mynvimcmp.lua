03c5abdd39a86eb0a4bbd39482c50a1e6198a5f5
Mehdi Slimani <mehdi.slimani@upc.edu>
2022-08-13 21:22:57 +0200

    First check if snippet is expandable

--- nvim/lua/mynvimcmp.lua
+++ nvim/lua/mynvimcmp.lua
@@ -29,10 +29,10 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
+      elseif cmp.visible() then
+        cmp.select_next_item()
       elseif has_words_before() then
         cmp.complete()
       else
