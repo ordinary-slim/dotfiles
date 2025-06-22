3b775d3e1c3585342e7c7deb47d72caaa8a60065
ordinary-slim <mehdi.s0kt@gmail.com>
2025-06-22 23:12:41 +0200

    feat(nvim): add maplocalleader and CodeCompanion keybindings
    - Set `vim.g.maplocalleader` to "\\" in `init.lua` for local leader mappings.
    - Configure `codecompanion.nvim` plugin with new keybindings:
      - `<C-a>`: Open action palette
      - `<Leader>a`: Toggle chat buffer
      - `<LocalLeader>a`: Add code to chat buffer (visual mode)

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,19 +1,32 @@
-return {'olimorris/codecompanion.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-          'nvim-telescope/telescope.nvim',
-        },
-        config = function()
-          require('codecompanion').setup({
-            -- Add your configuration here
-            -- For example:
-            -- default_provider = 'telescope',
-            -- providers = {
-            --   telescope = {
-            --     theme = 'dropdown',
-            --   },
-            -- },
-          })
-        end,
-}
+return
+  {'olimorris/codecompanion.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  keys = {
+    {
+      "<C-a>",
+      "<cmd>CodeCompanionActions<CR>",
+      desc = "Open the action palette",
+      mode = { "n", "v" },
+    },
+    {
+      "<Leader>a",
+      "<cmd>CodeCompanionChat Toggle<CR>",
+      desc = "Toggle a chat buffer",
+      mode = { "n", "v" },
+    },
+    {
+      "<LocalLeader>a",
+      "<cmd>CodeCompanionChat Add<CR>",
+      desc = "Add code to a chat buffer",
+      mode = { "v" },
+    },
+  },
+  config = function()
+    require('codecompanion').setup({
+    })
+  end,
+  }
