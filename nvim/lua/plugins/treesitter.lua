8097636f50c604f88a526165a5cad1d5b6674e63
ordinary-slim <mehdi-s0kt@gmail.com>
2025-06-23 10:36:15 +0200

    treesitter parsers and colorscheme change
    - Treesitter lazy loading
    - Most settings taken from `olimorris/dotfiles`

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,29 @@
-return { { 'nvim-treesitter/nvim-treesitter',
-          config = function()
-            vim.wo.foldmethod = 'expr'
-            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          end} }
+return {
+  {
+  'nvim-treesitter/nvim-treesitter', -- Smarter code understanding (syntax highlighting, folding, go to definition, etc.)
+  build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- parsers you want:
+      ensure_installed = {
+        "c", "cpp", "cmake", "diff", "gitcommit", "gitignore", "go",
+        "html", "json", "lua",
+        "markdown", "markdown_inline",
+        "python", "toml", "vim", "vimdoc", "yaml",
+      },
+
+      -- automatically download any that are missing
+      auto_install = true,      -- installs on buffer enter
+      sync_install = false,     -- set to true if you prefer :TSUpdateSync
+
+      highlight = { enable = true },
+      indent    = { enable = true },
+    })
+
+    vim.wo.foldmethod = "expr"
+    vim.wo.foldexpr   = "v:lua.vim.treesitter.foldexpr()"
+  end
+  }
+}
