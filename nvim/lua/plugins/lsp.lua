161410caf0bfe744b9f44d17e34be4a1ca3a6b08
ordinary-slim <mehdi-s0kt@gmail.com>
2025-07-28 12:36:58 +0200

    Automatic setup of LSPs installed by Mason - Was kindof already done but overshadowed by custom setup
    + cute icons for warnings and errors uwu

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,60 +19,94 @@ return {
       },
     },
     "mason-org/mason-lspconfig.nvim", -- Bridges the gap in names of LSPs and Mason package names
+    {
+      "ivanjermakov/troublesum.nvim", -- Diagnostic signs in the buffer
+      event = "LspAttach",
+      opts = {
+        enabled = function()
+          local ft = vim.bo.filetype
+          return ft ~= "lazy" and ft ~= "mason" and ft ~= "codecompanion"
+        end,
+        severity_format = vim
+          .iter(ipairs(icons))
+          :map(function(sev, icon)
+            return icon
+          end)
+          :totable(),
+      },
+    },
   },
   config = function()
     local nvim_lsp = require('lspconfig')
-    -- Instaleld servers
-    servers = { 'pyright', 'fortls', 'clangd', 'texlab',
-                'lemminx', 'cmake', 'dockerls', 'ltex_plus', 'bashls' }
     -- Use an on_attach function to only map the following keys
     -- after the language server attaches to the current buffer
-    local on_attach = function(client, bufnr)
-      local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-      -- Enable completion triggered by <c-x><c-o>
-      buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- LspAttach is where you enable features that only work
+    -- if there is a language server active in the file
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("dotfiles.lsp-attach", { clear = false }),
+      desc = "LSP actions",
+      callback = function(event)
+        -- print("LSP attached to buffer " .. event.buf)
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(event.buf, ...) end
+        -- Enable completion triggered by <c-x><c-o>
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-      -- Mappings.
-      local opts = { noremap=true, silent=true }
+        -- Mappings.
+        local opts = { noremap=true, silent=true }
 
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      local map = function(keys, desc, func, mode)
-        mode = mode or "n"
-        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-      end
-      map('gD', "", function() vim.lsp.buf.declaration() end, n)
-      map('gd', "", function() vim.lsp.buf.definition() end, n)
-      map('K', "", function() vim.lsp.buf.hover() end, n)
-      map('gi', "", function() vim.lsp.buf.implementation() end, n)
-      map('<C-,>', "", function() vim.lsp.buf.signature_help() end, n)
-      map('gr', "", function() vim.lsp.buf.references() end, n)
-      map('<space>wd', "", function() vim.lsp.buf.add_workspace_folder() end, n)
-      map('<space>wr', "", function() vim.lsp.buf.remove_workspace_folder() end, n)
-      map('<space>wl', "", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, n)
-      map('<space>D', "", function() vim.lsp.buf.type_definition() end, n)
-      map('<space>rn', "", function() vim.lsp.buf.rename() end, n)
-      map('<space>ca', "", function() vim.lsp.buf.code_action() end, n)
-      map('<space>e', "", function() vim.diagnostic.open_float() end, n)
-      map('[d', "", function() vim.diagnostic.goto_prev() end, n)
-      map(']d', "", function() vim.diagnostic.goto_next() end, n)
-      -- map('<space>q', "", function() vim.diagnostic.setloclist() end, n)
-      -- map('<space>f', "", function() vim.lsp.buf.formatting() end, n)
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local map = function(keys, desc, func, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+        map('gD', "", function() vim.lsp.buf.declaration() end, n)
+        map('gd', "", function() vim.lsp.buf.definition() end, n)
+        map('K', "", function() vim.lsp.buf.hover() end, n)
+        map('gi', "", function() vim.lsp.buf.implementation() end, n)
+        map('<C-,>', "", function() vim.lsp.buf.signature_help() end, n)
+        map('gr', "", function() vim.lsp.buf.references() end, n)
+        map('<space>wd', "", function() vim.lsp.buf.add_workspace_folder() end, n)
+        map('<space>wr', "", function() vim.lsp.buf.remove_workspace_folder() end, n)
+        map('<space>wl', "", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, n)
+        map('<space>D', "", function() vim.lsp.buf.type_definition() end, n)
+        map('<space>rn', "", function() vim.lsp.buf.rename() end, n)
+        map('<space>ca', "", function() vim.lsp.buf.code_action() end, n)
+        map('<space>e', "", function() vim.diagnostic.open_float() end, n)
+        map('[d', "", function() vim.diagnostic.goto_prev() end, n)
+        map(']d', "", function() vim.diagnostic.goto_next() end, n)
+        -- map('<space>q', "", function() vim.diagnostic.setloclist() end, n)
+        -- map('<space>f', "", function() vim.lsp.buf.formatting() end, n)
+    end,
+    })
 
-    end
+    require("mason-lspconfig").setup({
+      handlers = {
+        -- this first function is the "default handler"
+        -- it applies to every language server without a "custom handler"
+        function(ls)
+          require("lspconfig")[ls].setup({
+            capabilities = capabilities,
+            flags = {
+              debounce_text_changes = 150,
+            },
+          })
+        end,
+      },
+    })
 
-    -- Use a loop to conveniently call 'setup' on multiple servers and
-    -- map buffer local keybindings when the language server attaches
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()) -- completion
-
-    for _, lsp in ipairs(servers) do
-      nvim_lsp[lsp].setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        flags = {
-          debounce_text_changes = 150,
-        }
-      }
-    end
+    vim.diagnostic.config({
+      severity_sort = true,
+      signs = {
+        text = icons,
+      },
+      underline = false,
+      update_in_insert = true,
+      virtual_text = false,
+      -- virtual_text = {
+      --   prefix = "",
+      --   spacing = 0,
+      -- },
+    })
   end,
 }
