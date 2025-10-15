80bbcf90fa79fb7890065e2adb87d19ada148ca4
ordinary-slim <mehdi-s0kt@gmail.com>
2025-10-15 17:52:30 +0200

    i3!

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+  -- TODO list
+  {'bngarren/checkmate.nvim',
+    ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+    opts = {},
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
+}
