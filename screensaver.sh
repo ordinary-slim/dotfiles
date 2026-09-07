6614bb1e5aea017c34c3b119085687fa40956db5
ordinary-slim <mehdi.slimani.elmaddarsi@gmail.com>
2026-09-08 01:31:54 +0200

    hyprland config to lua

---
+++ screensaver.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+export SCREENSAVER="tte"
+export LOGO=$(cat $HOME/.config/flare.txt)
+
+exit_screensaver() {
+  hyprctl keyword cursor:invisible false &>/dev/null
+  pkill -9 -f ${SCREENSAVER}
+  pkill -f "ghostty --class=Screensaver"
+  exit 0
+}
+
+toggle_screensaver() {
+  if [ -f /tmp/no_screensaver ]; then
+    rm /tmp/no_screensaver
+    notify-send -t 5000 "Screensaver enabled"
+  else
+    touch /tmp/no_screensaver
+    notify-send -t 5000 "Screensaver disabled"
+  fi
+}
+
+is_running() {
+  if pgrep -f "ghostty --class=Screensaver"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+loop_screensaver() {
+
+  stty -echo -icanon time 0 min 0 </dev/tty
+
+  trap exit_screensaver SIGINT SIGTERM SIGHUP SIGQUIT
+
+  hyprctl keyword cursor:invisible true &>/dev/null
+
+  while true; do
+    tte -i ~/.config/flare.txt \
+      --frame-rate 60 --canvas-width 0 --canvas-height 0 --reuse-canvas --anchor-canvas c --anchor-text c\
+      --random-effect --include-effects thunderstorm laseretch \
+      --no-eol --no-restore-cursor &
+    while pgrep -x tte >/dev/null; do
+      if read -rs -n 1 -t 3; then
+        exit_screensaver
+      fi
+    done
+  done
+}
+
+launch_screensaver() {
+  if is_running || [ -f /tmp/no_screensaver ]; then
+    exit 0
+  fi
+
+  monitors=$(hyprctl monitors -j | jq -r '.[] | .name')
+
+  focused=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true).name')
+
+  for monitor in $monitors; do
+    # Focus the monitor so the new window opens there
+    hyprctl dispatch focusmonitor "$monitor"
+    hyprctl dispatch exec 'ghostty --class=Screensaver --title=Screensaver --fullscreen=true --cursor-color=black --command="$HOME/.config/screensaver.sh loop_screensaver"'
+  done
+
+  hyprctl dispatch focusmonitor $focused
+}
+
+$@
